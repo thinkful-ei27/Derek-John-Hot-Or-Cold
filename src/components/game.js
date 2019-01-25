3ed21e5098bc7ff211e38aed299cd452a1753cef
Derek Houck <derek@derekhouck.com>
2019-01-25 08:51:41 -0800

    Implement checkGuess function

--- src/components/game.js
+++ src/components/game.js
@@ -7,13 +7,19 @@ import GuessList from './guess-list';
 
 export default class Game extends React.Component {
     state = {
-        guesses: [10, 15, 25]
+        number: Math.floor(Math.random() * 100),
+        feedback: 'Make your guess!',
+        guesses: [10, 15, 25],
+        currentGuess: 0
+    }
+    checkGuess(guess) {
+        return guess === this.state.number ? this.setState({feedback: 'You won!'}) : this.setState({feedback: 'Try again'});
     }
     render () {
         return (
             <div>
                 <Header />
-                <GuessSection feedback="Make your guess!" />
+                <GuessSection feedback={this.state.feedback} handleClick={() => this.checkGuess(this.state.currentGuess)} />
                 <GuessCount count={this.state.guesses.length} />
                 <GuessList guesses={this.state.guesses} />
             </div>
--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -3,12 +3,16 @@ import React from 'react';
 import './guess-form.css';
 
 export default function GuessForm(props) {
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        props.handleClick();
+    };
     return (
-        <form>
+        <form onSubmit={(e) => handleFormSubmit(e)}>
             <input type="text" name="userGuess" id="userGuess"
                 className="text" maxLength="3" autoComplete="off"
                 placeholder="Enter your Guess" required />
-            <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
+            <input type="submit" id="guessButton" className="button" name="submit" value="Guess" />
         </form>
     );
 };
